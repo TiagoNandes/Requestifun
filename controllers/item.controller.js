25441dcd25b30142e0700f2e28139c9b3d95cd30
TiagoNandes <35899471+TiagoNandes@users.noreply.github.com>
2019-05-20 15:21:19 +0100

    Add files via upload

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -2,7 +2,7 @@
 const items = require('../models/item.model.js');
 
 function readItems(req, res) {
-    items.item.find({}, (err, result)=> {
+    items.item.find({}, (err, result) => {
         if (err) {
             res.send(err);
         } else {
@@ -12,7 +12,7 @@ function readItems(req, res) {
 }
 
 function readItemsId(req, res) {
-    items.item.findById(req.params.id, (err, result)=> {
+    items.item.findById(req.params.id, (err, result) => {
         if (err) {
             res.send(err);
         } else {
@@ -21,17 +21,18 @@ function readItemsId(req, res) {
     })
 }
 
-function saveItemshops(req,res){
+function saveItems(req, res) {
     let newItem = items.item({
-    
-    name: req.body.name,
-    state:req.body.state,
-    model:req.body.model,
-    urlImage:req.body.urlImage,
-    category:req.body.Category,
-    subCategory:req.body.subCategory,
+
+        name: req.body.name,
+        state: req.body.state,
+        model: req.body.model,
+        code: req.body.code,
+        urlImage: req.body.urlImage,
+        category: req.body.category,
+        subCategory: req.body.subCategory,
     })
-    newItem.save((err)=> {
+    newItem.save((err) => {
         if (err) {
             res.send(err);
         } else {
@@ -40,9 +41,59 @@ function saveItemshops(req,res){
     })
 }
 
+function updateItems(req, res) {
+    items.item.findById(req.params.id, (err, item) => {
+        if (err) res.send(err);
+        else {
+            for (let item in req.body) {
+                if (item[item] || item[item] == null) {
+                    item[item] = req.body[item];
+                }
+            }
+            item.save((err) => {
+                if (err) res.send(err);
+                else res.send("Success")
+            })
+        }
+    })
+
+}
+
+function deleteLogicItems(req, res) {
+    items.item.findOneAndUpdate({ _id: req.params.id }, { state: "Not available" }, (err) => {
+        if (err) res.send(err)
+        else res.send("O item foi desativado.")
+    })
+}
+
+function deleteFisicItems(req, res) {
+    items.item.deleteMany({}, (err) => {
+        if (err) res.send(err)
+        else res.send("Todos os items foram apagados.")
+    })
+}
+
+function deleteFisicItemsId(req, res) {
+    items.item.findOneAndDelete({ _id: req.params.id }, (err) => {
+        if (err) res.send(err)
+        else res.send("O item foi apagado com sucesso.")
+    })
+}
+
+function reactivateLogicItems(req, res) {
+    items.item.findOneAndUpdate({ _id: req.params.id }, { state: "Available" }, (err) => {
+        if (err) res.send(err)
+        else res.send("O utilizador foi reativado.")
+    })
+}
+
 module.exports = {
     readItems: readItems,
     readItemsId: readItemsId,
-    saveItems: saveItems
-
+    saveItems: saveItems,
+    updateItems: updateItems,
+    deleteLogicItems: deleteLogicItems,
+    deleteFisicItems: deleteFisicItems,
+    deleteFisicItemsId: deleteFisicItemsId,
+    reactivateLogicItems: reactivateLogicItems,
 };
--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 //require do controller
-const router= require('express').Router();
+const router = require('express').Router();
 const controllerUsers = require('./controllers/users.controller.js');
 const controllerAuth = require('./controllers/auth.controller.js');
 const controllerItems = require('./controllers/item.controller.js');
 const jwt = require('./assets/scripts/jwt.js');
 
-router.get('/', function(req,res){
+router.get('/', function(req, res) {
     res.send(console.log("A definir posteriormente com o professor."));
     res.end();
 });
@@ -14,7 +14,7 @@ router.get('/', function(req,res){
 router.post('/login', controllerAuth.login);
 
 //tokens
-router.get('/token', (req,res) => jwt.validateToken(req, res, controllerUsers.readUsers));
+router.get('/token', (req, res) => jwt.validateToken(req, res, controllerUsers.readUsers));
 //router.get('/refreshtoken', jwt.refreshToken);
 
 //rota para users
@@ -25,13 +25,19 @@ router.put('/users/:id', controllerUsers.updateUsers);
 //router.delete('/users', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicUsers));
 //router.delete('/users/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicUsersId));
 //router.put('/users/delete/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteLogicUsers));
-router.put('/users/delete/:id',  controllerUsers.deleteLogicUsers);
-router.put('/users/activate/:id',  controllerUsers.reactivateLogicUsers);
+router.put('/users/delete/:id', controllerUsers.deleteLogicUsers);
+router.put('/users/activate/:id', controllerUsers.reactivateLogicUsers);
 
 //Rotas para items
 router.get('/items/', controllerItems.readItems);
 router.get('/items/:id', controllerItems.readItemsId);
 router.post('/items/', controllerItems.saveItems);
+router.put('/items/:id', controllerItems.updateItems);
+//router.delete('/items', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicItems));
+//router.delete('/items/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicItemsId));
+//router.put('/items/delete/:id', (req,res) => jwt.validateToken(req, res, controllerItems.deleteLogicItems));
+router.put('/items/delete/:id', controllerItems.deleteLogicItems);
+router.put('/items/activate/:id', controllerItems.reactivateLogicItems);
 
 //exportar router
 module.exports = router;
