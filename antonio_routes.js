1fb7dfb41b9a703603a2dc56c540fde59e4e97c6
9150408 <37103814+9150408@users.noreply.github.com>
2019-05-20 12:17:33 +0100

    Add files via upload

---
+++ antonio_routes.js
@@ -0,0 +1,43 @@
+//require do controller
+const router = require('express').Router();
+const controllerUsers = require('./controllers/users.controller.js');
+const controllerAuth = require('./controllers/auth.controller.js');
+const controllerItems = require('./controllers/item.controller.js');
+const jwt = require('./assets/scripts/jwt.js');
+
+router.get('/', function(req, res) {
+    res.send(console.log("A definir posteriormente com o professor."));
+    res.end();
+});
+
+//rotas para autenticação
+router.post('/login', controllerAuth.login);
+
+//tokens
+router.get('/token', (req, res) => jwt.validateToken(req, res, controllerUsers.readUsers));
+//router.get('/refreshtoken', jwt.refreshToken);
+
+//rota para users
+router.get('/users', controllerUsers.readUsers);
+router.get('/users/:id', controllerUsers.readUsersId);
+router.post('/users', controllerUsers.saveUsers);
+router.put('/users/:id', controllerUsers.updateUsers);
+//router.delete('/users', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicUsers));
+//router.delete('/users/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicUsersId));
+//router.put('/users/delete/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteLogicUsers));
+router.put('/users/delete/:id', controllerUsers.deleteLogicUsers);
+router.put('/users/activate/:id', controllerUsers.reactivateLogicUsers);
+
+//Rotas para items
+router.get('/items/', controllerItems.readItems);
+router.get('/items/:id', controllerItems.readItemsId);
+router.post('/items/', controllerItems.saveItems);
+router.put('/items/:id', controllerItems.updateItems);
+//router.delete('/items', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicItems));
+//router.delete('/items/:id', (req,res) => jwt.validateToken(req, res, controllerUsers.deleteFisicItemsId));
+//router.put('/items/delete/:id', (req,res) => jwt.validateToken(req, res, controllerItems.deleteLogicItems));
+router.put('/items/delete/:id', controllerItems.deleteLogicItems);
+router.put('/items/activate/:id', controllerItems.reactivateLogicItems);
+
+//exportar router
+module.exports = router;
